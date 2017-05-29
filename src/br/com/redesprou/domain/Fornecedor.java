c2effc5dc0ef7ed14ea123f4c08573149aad90f1
Erivan <erivan.nas@gmail.com>
2017-05-29 07:29:52 -0300

    Adiciona classes de dominio

---
+++ src/br/com/redesprou/domain/Fornecedor.java
@@ -0,0 +1,31 @@
+package br.com.redesprou.domain;
+
+public class Fornecedor {
+	private Long codigo;
+	private String razaoSocial;
+	private String cnpj;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+}
---
+++ src/br/com/redesprou/domain/Produto.java
@@ -0,0 +1,31 @@
+package br.com.redesprou.domain;
+
+public class Produto {
+	private Long codigo;
+	private String nome;
+	private Fornecedor fornecedor;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Fornecedor getFornecedor() {
+		return fornecedor;
+	}
+
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+}
