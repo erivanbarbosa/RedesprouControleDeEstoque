e63792ef0b397e7d4e3b074cc18f25c783c02cd1
Erivan <erivan.nas@gmail.com>
2017-05-29 10:01:05 -0300

    Adiciona Tempate modeloSistema
    Modelo usado como esqueleto para páginas xhtml

--- src/br/com/redesprou/dao/ClienteDAO.java
+++ src/br/com/redesprou/dao/ClienteDAO.java
@@ -53,138 +53,82 @@ public class ClienteDAO {
 
 		comando.executeUpdate();
 	}
-	
-	public Cliente buscarPorCodigo(Cliente cliente) throws SQLException{
+
+	public Cliente buscarPorCodigo(Cliente cliente) throws SQLException {
 		StringBuilder sql = new StringBuilder();
 		sql.append("SELECT codigo, razao_social, cnpj ");
 		sql.append("FROM cliente ");
-		sql.append( "WHERE codigo = ? ");
-		
+		sql.append("WHERE codigo = ? ");
+
 		Connection conexao = ConexaoFactory.conectar();
 
 		PreparedStatement comando = conexao.prepareStatement(sql.toString());
 		comando.setLong(1, cliente.getCodigo());
 
 		ResultSet resultado = comando.executeQuery();
-		
+
 		Cliente retorno = null;
-		
-		if( resultado.next() ){
+
+		if (resultado.next()) {
 			retorno = new Cliente();
-			retorno.setCodigo( resultado.getLong("codigo"));
+			retorno.setCodigo(resultado.getLong("codigo"));
 			retorno.setRazaoSocial(resultado.getString("razao_social"));
 			retorno.setCnpj(resultado.getString("cnpj"));
 		}
-		
+
 		return retorno;
 	}
-	
-	public ArrayList<Cliente> listar() throws SQLException{
+
+	public ArrayList<Cliente> listar() throws SQLException {
 		StringBuilder sql = new StringBuilder();
 		sql.append("SELECT codigo, razao_social, cnpj ");
-		sql.append( "FROM cliente " );
+		sql.append("FROM cliente ");
 		sql.append("ORDER BY codigo ASC ");
-		
+
 		Connection conexao = ConexaoFactory.conectar();
 
 		PreparedStatement comando = conexao.prepareStatement(sql.toString());
 		ResultSet resultado = comando.executeQuery();
-		
+
 		ArrayList<Cliente> lista = new ArrayList<Cliente>();
-		
-		while(resultado.next() )
-		{
-			Cliente c1 = new Cliente();
-			c1.setCodigo( resultado.getLong("codigo"));
-			c1.setRazaoSocial(resultado.getString("razao_social"));
-			c1.setCnpj(resultado.getString("cnpj"));
-			
-			lista.add(c1);
-		}
-		
-		return lista;
-	}
-	
-	public ArrayList<Cliente> listarPorDescricao(Cliente cliente ) throws SQLException
-	{
-		StringBuilder sql = new StringBuilder();
-		sql.append("SELECT codigo, razao_social, cnpj " );
-		sql.append("FROM cliente ");
-		sql.append("WHERE razao_social LIKE ? ");
-		sql.append("ORDER BY codigo ASC ");
-		
-		Connection conexao = ConexaoFactory.conectar();
-		PreparedStatement comando = conexao.prepareStatement( sql.toString() );
-		comando.setString(1, "%" + cliente.getRazaoSocial() + "%");
-		
-		ResultSet resultado = comando.executeQuery();
-		ArrayList<Cliente> lista = new ArrayList<Cliente>();
-		
-		while( resultado.next() )
-		{
+
+		while (resultado.next()) {
 			Cliente c1 = new Cliente();
 			c1.setCodigo(resultado.getLong("codigo"));
 			c1.setRazaoSocial(resultado.getString("razao_social"));
 			c1.setCnpj(resultado.getString("cnpj"));
-			
+
 			lista.add(c1);
 		}
-		
-		return lista;
-		
-	}
-	
-	public static void main(String[] args )
-	{
-		/*
-		Cliente c1 = new Cliente();
-		Cliente c2 = new Cliente();
-		c1.setCodigo(1L);
-		c2.setCodigo(5L);
-		
-		ClienteDAO cDAO = new ClienteDAO();
-		
-		try {
-			Cliente r1 = cDAO.buscarPorCodigo(c1);
-			Cliente r2 = cDAO.buscarPorCodigo(c2);
-			System.out.println("Deu certo");
-			
-			System.out.println("result 1: " + r1 );
-			System.out.println("result 1: " + r2 );
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println("Nao deu certo");
-		}*/
-		
-		/*
-		ClienteDAO cDAO = new ClienteDAO();
-		try {
-			ArrayList<Cliente> lista = cDAO.listar();
-			
-			for (Cliente c : lista )
-			{
-				System.out.println(c);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println("Erro");
-		}*/
-		
-		Cliente c1 = new Cliente();
-		c1.setRazaoSocial("Arcon");
-		
-		ClienteDAO cDAO = new ClienteDAO();
-		try {
-			ArrayList<Cliente> lista = cDAO.listarPorDescricao(c1);
-			
-			for (Cliente c : lista )
-			{
-				System.out.println(c);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println("Erro");
+		return lista;
+	}
+
+	public ArrayList<Cliente> listarPorDescricao(Cliente cliente)
+			throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT codigo, razao_social, cnpj ");
+		sql.append("FROM cliente ");
+		sql.append("WHERE razao_social LIKE ? ");
+		sql.append("ORDER BY codigo ASC ");
+
+		Connection conexao = ConexaoFactory.conectar();
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setString(1, "%" + cliente.getRazaoSocial() + "%");
+
+		ResultSet resultado = comando.executeQuery();
+		ArrayList<Cliente> lista = new ArrayList<Cliente>();
+
+		while (resultado.next()) {
+			Cliente c1 = new Cliente();
+			c1.setCodigo(resultado.getLong("codigo"));
+			c1.setRazaoSocial(resultado.getString("razao_social"));
+			c1.setCnpj(resultado.getString("cnpj"));
+
+			lista.add(c1);
 		}
+
+		return lista;
+
 	}
 }
