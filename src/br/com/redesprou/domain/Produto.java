0c076f1649ae69605104a5d5b974204100bcca98
Erivan <erivan.nas@gmail.com>
2017-05-30 10:00:45 -0300

    Atualiza classe produto
    Classe atualizada para incluir nos novos atributos do tipo produto.

--- src/br/com/redesprou/domain/Produto.java
+++ src/br/com/redesprou/domain/Produto.java
@@ -2,7 +2,9 @@ package br.com.redesprou.domain;
 
 public class Produto {
 	private Long codigo;
-	private String nome;
+	private String descricao;
+	private Double preco;
+	private Long quantidade;
 	private Fornecedor fornecedor;
 
 	public Long getCodigo() {
@@ -13,12 +15,28 @@ public class Produto {
 		this.codigo = codigo;
 	}
 
-	public String getNome() {
-		return nome;
+	public String getDescricao() {
+		return descricao;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Long getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Long quantidade) {
+		this.quantidade = quantidade;
 	}
 
 	public Fornecedor getFornecedor() {
