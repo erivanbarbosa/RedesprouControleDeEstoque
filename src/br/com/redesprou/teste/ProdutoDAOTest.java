34526ef367fd6b8bebcf1c43e34bdc5a0f841c49
Erivan <erivan.nas@gmail.com>
2017-05-30 10:55:57 -0300

    Atualiza teste produtoDAO
    Adicina teste para o método listar

--- src/br/com/redesprou/teste/ProdutoDAOTest.java
+++ src/br/com/redesprou/teste/ProdutoDAOTest.java
@@ -3,6 +3,9 @@ package br.com.redesprou.teste;
 import static org.junit.Assert.*;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
 
 import org.junit.Test;
 
@@ -13,6 +16,7 @@ import br.com.redesprou.domain.Produto;
 public class ProdutoDAOTest {
 
 	@Test
+	@Ignore
 	public void salvar() throws SQLException {
 		Produto p = new Produto();
 		Fornecedor f = new Fornecedor();
@@ -24,7 +28,24 @@ public class ProdutoDAOTest {
 		p.setFornecedor(f);
 		
 		ProdutoDAO dao = new ProdutoDAO();
-		dao.salvar(p);
+		//dao.salvar(p);
+	}
+	
+	@Test
+	public void listar() throws SQLException {
+		ProdutoDAO dao = new ProdutoDAO();
+		ArrayList<Produto> lista = dao.listar();
+		
+		for(Produto p : lista ){
+			System.out.println("Codigo do produto: " + p.getCodigo() );
+			System.out.println("Descrição do produto: " + p.getDescricao());
+			System.out.println("Preço do produto: " + p.getPreco());
+			System.out.println("Quantidade do produto: " + p.getQuantidade());
+			System.out.println("Codigo do fornecedor " + p.getFornecedor().getCodigo() );
+			System.out.println("Razao Social do fornecedor: " + p.getFornecedor().getRazaoSocial());
+			System.out.println("CNPJ do fornecedor: " + p.getFornecedor().getCnpj());
+			System.out.println();
+		}
 	}
 
 }
