8f3e9cb7619c78cc0315fdf1748dd7c5d9dfde36
Erivan <erivan.nas@gmail.com>
2017-05-31 13:12:44 -0300

    Atualiza classe ProdutoDAO
    Adicionado o método editar

--- src/br/com/redesprou/dao/ProdutoDAO.java
+++ src/br/com/redesprou/dao/ProdutoDAO.java
@@ -27,53 +27,67 @@ public class ProdutoDAO {
 
 		comando.executeUpdate();
 	}
-	
-	public ArrayList<Produto> listar() throws SQLException{
+
+	public ArrayList<Produto> listar() throws SQLException {
 		StringBuilder sql = new StringBuilder();
 		sql.append("SELECT p.codigo, p.descricao, p.preco, p.quantidade, f.codigo, f.razao_social, f.cnpj ");
 		sql.append("FROM produto p ");
 		sql.append("INNER JOIN fornecedor f ON f.codigo = p.fornecedor_codigo ");
-		
+
 		Connection conexao = ConexaoFactory.conectar();
 		PreparedStatement comando = conexao.prepareStatement(sql.toString());
-		
+
 		ResultSet resultado = comando.executeQuery();
-		
+
 		ArrayList<Produto> itens = new ArrayList<Produto>();
-		
-		while(resultado.next() )
-		{
+
+		while (resultado.next()) {
 			Fornecedor f = new Fornecedor();
-			f.setCodigo( resultado.getLong("f.codigo"));
+			f.setCodigo(resultado.getLong("f.codigo"));
 			f.setRazaoSocial(resultado.getString("f.razao_social"));
 			f.setCnpj(resultado.getString("f.cnpj"));
-			
+
 			Produto p = new Produto();
-			p.setCodigo( resultado.getLong("p.codigo"));
+			p.setCodigo(resultado.getLong("p.codigo"));
 			p.setDescricao(resultado.getString("p.descricao"));
 			p.setPreco(resultado.getDouble("p.preco"));
 			p.setQuantidade(resultado.getLong("p.quantidade"));
 			p.setFornecedor(f);
-			
+
 			itens.add(p);
 		}
-		
+
 		return itens;
 	}
-	
-	public void excluir(Produto p ) throws SQLException
-	{
+
+	public void excluir(Produto p) throws SQLException {
 		StringBuilder sql = new StringBuilder();
-		sql.append( "DELETE FROM produto ");
+		sql.append("DELETE FROM produto ");
+		sql.append("WHERE codigo = ? ");
+
+		Connection conexao = ConexaoFactory.conectar();
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+
+		comando.setLong(1, p.getCodigo());
+
+		comando.executeUpdate();
+	}
+
+	public void editar(Produto p) throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append( "UPDATE produto ");
+		sql.append("SET descricao = ?, preco = ?, quantidade = ?, fornecedor_codigo = ? ");
 		sql.append("WHERE codigo = ? ");
 		
 		Connection conexao = ConexaoFactory.conectar();
 		PreparedStatement comando = conexao.prepareStatement( sql.toString() );
 		
-		comando.setLong(1, p.getCodigo());
+		comando.setString(1, p.getDescricao());
+		comando.setDouble(2, p.getPreco());
+		comando.setLong(3, p.getQuantidade());
+		comando.setLong(4, p.getFornecedor().getCodigo());
 		
-		comando.executeUpdate();
+		comando.executeLargeUpdate();
 	}
-	
-	
+
 }
