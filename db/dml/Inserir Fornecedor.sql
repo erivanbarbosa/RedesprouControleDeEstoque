33ac120403fdec0f0cf41edc1f9c3aaf71b4127b
Erivan <erivan.nas@gmail.com>
2017-06-01 09:18:15 -0300

    Atualiza DML Inserir Fornecedor
    Campo código removido do script de inserção

--- db/dml/Inserir Fornecedor.sql
+++ db/dml/Inserir Fornecedor.sql
@@ -1,1 +1,1 @@
-INSERT INTO `controle_de_estoque`.`fornecedor` (`codigo`, `razao_social`, `cnpj`) VALUES ('1', 'Neo Quimica', '7877888897');
+INSERT INTO `controle_de_estoque`.`fornecedor` (`razao_social`, `cnpj`) VALUES ('Neo Quimica', '7877888897');
