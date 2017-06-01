e416dc737fb96653aefff46f942c8516ba56cade
Erivan <erivan.nas@gmail.com>
2017-06-01 10:01:01 -0300

    Adiciona Classe ProdutoBean

---
+++ src/br/com/redesprou/bean/ProdutoBean.java
@@ -0,0 +1,35 @@
+package br.com.redesprou.bean;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.redesprou.dao.ProdutoDAO;
+import br.com.redesprou.domain.Produto;
+import br.com.redesprou.util.JSFUtil;
+
+@ManagedBean(name = "MBProduto")
+@ViewScoped
+public class ProdutoBean {
+	private ArrayList<Produto> listaDeProdutos;
+
+	public ArrayList<Produto> getListaDeProdutos() {
+		return listaDeProdutos;
+	}
+
+	public void setListaDeProdutos(ArrayList<Produto> listaDeProdutos) {
+		this.listaDeProdutos = listaDeProdutos;
+	}
+
+	public void prepararListagem() {
+		try {
+			ProdutoDAO dao = new ProdutoDAO();
+			listaDeProdutos = dao.listar();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			JSFUtil.adicionarMensagemErro(e.getMessage());
+		}
+	}
+}
